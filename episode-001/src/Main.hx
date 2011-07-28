1a77758296570e99f5eafd0edb47545e79ebedda
synthmax <max@koblo.com>
2011-07-28 15:08:54 +0200

    initial commit

---
+++ episode-001/src/Main.hx
@@ -0,0 +1,20 @@
+package;
+
+import flash.display.MovieClip;
+import flash.Lib;
+
+
+class Main
+{
+	static function main()
+	{
+		new Main();
+	
+	}
+	
+	public function new()
+	{	
+		trace('Hello World!');
+	}
+
+}
